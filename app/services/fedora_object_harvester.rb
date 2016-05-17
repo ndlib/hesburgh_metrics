5e4a13c525646716e674f27129fe65015ab77693
LaRita Robinson <lrobins5@nd.edu>
2016-05-17 10:23:53 -0400

    Minor style corrections to fedora_object_harvester

--- app/services/fedora_object_harvester.rb
+++ app/services/fedora_object_harvester.rb
@@ -15,8 +15,7 @@ class FedoraObjectHarvester
   end
   # query for objects and process one result at a time
   def harvest
-    repo.search 'pid~und:*' do  |doc|
-
+    @repo.search 'pid~und:*' do |doc|
       pid = doc.pid
       af_model = get_af_model(doc)
       resource_type = get_resource_type(doc) || af_model
@@ -27,7 +26,7 @@ class FedoraObjectHarvester
       obj_modified_date = doc.profile["objLastModDate"]
       access_rights = get_access_rights(doc)
       fedora_object = FedoraObject.create!(pid: pid, af_model: af_model, resource_type: resource_type, mimetype: mimetype, bytes: bytes, parent_pid: parent_pid, obj_injest_date: obj_injest_date, obj_modified_date: obj_modified_date, access_rights: access_rights)
-      get_aggregation_keys(doc, fedora_object)
+      get_and_assign_aggregation_keys(doc, fedora_object)
       end
     end
   end
@@ -42,8 +41,8 @@ class FedoraObjectHarvester
     # for resource_type, use = dc/terms/type...
     # <info:fedora/und:02870v8524d> <http://purl.org/dc/terms/type> "GenericFile" .
   def get_resource_type(doc)
-    resource_type = parse_triples(doc.datastreams['descMetadata'].content, 'type')
-    return resource_type[0] # this is an array but should only have one
+    resource_types = parse_triples(doc.datastreams['descMetadata'].content, 'type')
+    return resource_types[0] # this is an array but should only have one
   end
 
   # if content datastream exists, use mimetype of datastream, else nil
@@ -56,13 +55,13 @@ class FedoraObjectHarvester
   end
 
   # parse from XML <ns2:isPartOf rdf:resource='info:fedora/und:02870v85143' />
-  def get_parent_pid
+  def get_parent_pid(doc)
     parse_xml_RELSEXT(doc.datastreams['RELS-EXT'].content, 'isPartOf')
   end
 
   # parse from triples: creator#administrative_unit
   # <info:fedora/und:7h149p31207> <http://purl.org/dc/terms/creator#administrative_unit> "University of Notre Dame::College of Science::Non-Departmental" .
-  def get_aggregation_keys(doc, fedora_object)
+  def get_and_assign_aggregation_keys(doc, fedora_object)
     agg_key_array = parse_triples(doc.datastreams['descMetadata'].content, 'creator#administrative_unit')
     agg_key_array.each do |aggregation_key|
       fedora_object.fedora_object_aggregation_keys.create!(aggregation_key: aggregation_key)
@@ -83,9 +82,9 @@ class FedoraObjectHarvester
   ## ===========
   #<access type="read"><human/><machine><group>public</group></machine></access>
   def read_rights(this_access)
-    machine_group_rights=this_access.elements['machine'].elements['group']
+    machine_group_rights = this_access.elements['machine'].elements['group']
     if !machine_group_rights.nil?
-      case  machine_group_rights.first
+      case machine_group_rights.first
       when 'public'
         'public'
       when 'registered'
@@ -95,13 +94,13 @@ class FedoraObjectHarvester
       end
     else
       'private'
-    end #end if
+    end
   end
   ## ===========
   # <embargo><human/><machine><date>2016-06-01</date></machine></embargo>
     # concatenate (embargo) onto prior string if it exists
   def embargo_rights(this_access)
-    machine_group_rights=this_access.elements['machine'].elements['group']
+    machine_group_rights = this_access.elements['machine'].elements['group']
     if !machine_group_rights.nil?
       if !machine_group_rights.elements['date'].first.nil?
         ' (embargo)'
@@ -113,7 +112,7 @@ class FedoraObjectHarvester
   # <ns2:isPartOf rdf:resource='info:fedora/und:02870v85143' />
   def parse_xml_RELSEXT(stream, search_key)
     # RELS-EXT is RDF-XML - parse it
-  xml_hash = {}
+    xml_hash = {}
     RDF::RDFXML::Reader.new(stream).each do |thing|
       key = thing.predicate.to_s.split("#")
       if key[1] == search_key
@@ -122,7 +121,7 @@ class FedoraObjectHarvester
         break
       end
     end
-  return xml_hash[search_key]
+    return xml_hash[search_key]
   end
   ## ============================================================================
   # Parse triples format, returning an array of all values matching the search key.
@@ -143,5 +142,5 @@ class FedoraObjectHarvester
 
     return data_array
   end
-## ============================================================================
+  ## ============================================================================
 end
