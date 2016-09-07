6fabbb6e58c307135c3984bf9785dc2852a7439c
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-09-07 16:06:54 -0400

    Setting Airbrake environment to `Rails.env`

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -2,4 +2,5 @@ Airbrake.configure do |config|
   config.host = Figaro.env.airbrake_host!
   config.project_id = true
   config.project_key = Figaro.env.airbrake_project_key!
+  config.environment = Rails.env
 end
