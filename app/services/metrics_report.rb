e2cb002635ca367464e3b18d6468f5d9dc270d78
Banurekha <banurekha.l@gmail.com>
2017-01-24 10:19:36 -0500

    Reworking on getting storage information from database
    Generate storage information for both fedora and bendo. Adding error handling as well

--- app/services/metrics_report.rb
+++ app/services/metrics_report.rb
@@ -2,35 +2,60 @@ require 'erb'
 class MetricsReport
 
   class MetricsDetail
-    attr_accessor :report_start_date, :report_end_date, :fedora_count, :fedora_size
+    attr_accessor :report_start_date, :report_end_date, :fedora_count, :fedora_size, :storage
     def initialize(report_start_date,report_end_date )
       @report_start_date = report_start_date
       @report_end_date = report_end_date
     end
   end
 
-  attr_reader :metrics, :exceptions, :fedora, :filename
+  attr_reader :metrics, :exceptions, :filename
 
   def initialize(report_start_date, report_end_date)
     @exceptions = []
     @metrics = MetricsDetail.new(report_start_date, report_end_date)
-    @fedora = "Fedora"
-    @filename = "CurateND-report-#{report_start_date}-through-#{report_end_date}.md"
-    @template = File.read(File.join(Rails.root, 'app','templates','periodic_metrics.txt.erb'))
-    fedora_storage_information
+    @filename = "CurateND-report-#{report_start_date}-through-#{report_end_date}.html"
+    @template = File.read(File.join(Rails.root, 'app','templates','periodic_metrics.html.erb'))
   end
 
-  def fedora_storage_information
-    metrics.fedora_count = CurateStorageDetail.where(harvest_date: metrics.report_end_date, storage_type: fedora).last.object_count
-    metrics.fedora_size = CurateStorageDetail.where(harvest_date: metrics.report_end_date, storage_type: fedora).last.object_bytes
+  def generate_report
+    #Storage Information
+    @metrics.storage = []
+      begin
+        ['Fedora', 'Bendo'].map do |type|
+          @metrics.storage << storage_information_for(type)
+        end
+        save!
+      rescue StandardError => e
+        @exceptions << "PID: #{doc.pid} -- #{e.inspect}"
+      end
+    report_any_exceptions
   end
 
+  private
+
+  def report_any_exceptions
+    return unless @exceptions.any?
+    @exceptions.each do |error_message|
+      Airbrake.notify_sync(
+          'MetricsReportError',
+          errors: error_message
+      )
+    end
+  end
+
+  def storage_information_for(storage_type)
+    h = {}
+    storage = CurateStorageDetail.where(harvest_date: metrics.report_end_date, storage_type: storage_type).last
+    h[storage_type] = {count: storage.object_count, size: storage.object_bytes} if storage.present?
+    h
+  end
 
   def render
     ERB.new(@template).result( binding )
   end
 
-  def save
+  def save!
     File.open(filename, "w+") do |f|
       f.write(render)
     end
