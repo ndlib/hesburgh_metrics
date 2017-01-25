6ce12b052c55e7ff108967e7fc63a374035dabe8
Banurekha <banurekha.l@gmail.com>
2017-01-25 12:46:34 -0500

    Adding spec and changes related it

--- app/services/metrics_report.rb
+++ app/services/metrics_report.rb
@@ -1,9 +1,8 @@
 require 'erb'
 class MetricsReport
-
   class MetricsDetail
     attr_accessor :report_start_date, :report_end_date, :fedora_count, :fedora_size, :storage
-    def initialize(report_start_date,report_end_date )
+    def initialize(report_start_date, report_end_date)
       @report_start_date = report_start_date
       @report_end_date = report_end_date
     end
@@ -15,20 +14,20 @@ class MetricsReport
     @exceptions = []
     @metrics = MetricsDetail.new(report_start_date, report_end_date)
     @filename = "CurateND-report-#{report_start_date}-through-#{report_end_date}.html"
-    @template = File.read(File.join(Rails.root, 'app','templates','periodic_metrics.html.erb'))
+    @template = File.read(File.join(Rails.root, 'app', 'templates', 'periodic_metrics.html.erb'))
   end
 
   def generate_report
-    #Storage Information
+    # Storage Information
     @metrics.storage = []
-      begin
-        ['Fedora', 'Bendo'].map do |type|
-          @metrics.storage << storage_information_for(type)
-        end
-        save!
-      rescue StandardError => e
-        @exceptions << "PID: #{doc.pid} -- #{e.inspect}"
+    begin
+      %w(Fedora Bendo).map do |type|
+        @metrics.storage << storage_information_for(type)
       end
+      save!
+    rescue StandardError => e
+      @exceptions << e.inspect.to_s
+    end
     report_any_exceptions
   end
 
@@ -38,8 +37,8 @@ class MetricsReport
     return unless @exceptions.any?
     @exceptions.each do |error_message|
       Airbrake.notify_sync(
-          'MetricsReportError',
-          errors: error_message
+        'MetricsReportError',
+        errors: error_message
       )
     end
   end
@@ -47,18 +46,17 @@ class MetricsReport
   def storage_information_for(storage_type)
     h = {}
     storage = CurateStorageDetail.where(harvest_date: metrics.report_end_date, storage_type: storage_type).last
-    h[storage_type] = {count: storage.object_count, size: storage.object_bytes} if storage.present?
+    h[storage_type] = { count: storage.object_count, size: storage.object_bytes } if storage.present?
     h
   end
 
   def render
-    ERB.new(@template).result( binding )
+    ERB.new(@template).result(binding)
   end
 
   def save!
-    File.open(filename, "w+") do |f|
+    File.open(filename, 'w+') do |f|
       f.write(render)
     end
   end
-
 end
---
+++ spec/services/metrics_report_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe MetricsReport do
+  context '#generate_report' do
+    let (:start_date) { Date.today - 7 }
+    let (:end_date) { Date.today }
+    let(:report) { described_class.new(start_date, end_date) }
+    subject { report.generate_report }
+
+    before do
+      CurateStorageDetail.create(id: 1, harvest_date: end_date, storage_type: 'Fedora',
+                                 object_count: 100, object_bytes: 123_456)
+      CurateStorageDetail.create(id: 2, harvest_date: end_date, storage_type: 'Bendo',
+                                 object_count: 10, object_bytes: 1234)
+    end
+
+    it 'generate metrics report for given reporting dates', functional: true do
+      subject
+      expect(report.metrics.storage.count).to eq(2)
+    end
+
+    it 'will report to Airbrake any exceptions encountered' do
+      allow(report).to receive(:save!).and_raise(RuntimeError)
+      expect(Airbrake).to receive(:notify_sync).and_call_original
+      subject
+    end
+  end
+end
