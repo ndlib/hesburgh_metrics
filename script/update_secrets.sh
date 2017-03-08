6f8bfc0d0e662798f195343d206816286a75bacd
msuhovec <suhovecky@nd.edu>
2017-03-08 16:52:58 -0500

    change config target dir in update_secrets.sh
    #65

--- script/update_secrets.sh
+++ script/update_secrets.sh
@@ -16,7 +16,7 @@ for f in $files_to_copy; do
     echo "=-=-=-=-=-=-=-= copy $f"
     if [ -f ${secret_dir}/$f ];
     then
-        cp ${secret_dir}/$f $f
+        cp ${secret_dir}/$f config/$f
     else
         echo "Fatal Error: File $f does not exist in $secret_dir"
         exit 1
