721abce533f5ac1c74686512832116017cee5230
Mark Suhovecky <msuhovec@nd.edu>
2020-11-11 10:53:10 -0500

    curate-212
    use curatesvr-prod.lc.nd.edu instead of curatesvr-prod.library.nd.edu for ssh

--- config/deploy.rb
+++ config/deploy.rb
@@ -202,7 +202,7 @@ task :production do
 
  default_environment['PATH'] = "#{ruby_root}/root/usr/local/bin:/opt/rh/nodejs010/root/usr/bin:$PATH"
  default_environment['LD_LIBRARY_PATH'] = "#{ruby_root}/root/lib64:/opt/rh/nodejs010/root/lib64:/opt/rh/v8314/root/lib64:$LD_LIBRARY_PATH"
-  server 'app@curatesvr-prod.library.nd.edu', :app, :web, :db, primary: true
+  server 'app@curatesvr-prod.lc.nd.edu', :app, :web, :db, primary: true
 
   after 'deploy:update_code', 'und:write_env_vars', 'und:write_build_identifier', 'und:update_secrets', 'deploy:symlink_update', 'deploy:migrate', 'db:seed'
   after 'deploy', 'deploy:cleanup'
